a6157748c349e5c1df580333b8502721dbe99d98
zoltan2561 <pappzoltan6969@gmailcom>
2023-11-29 21:43:19 +0100

    cifix2

--- src/test/java/com/example/notes/BooksApplicationTests.java
+++ src/test/java/com/example/notes/BooksApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.notes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BooksApplication.class)
 class BooksApplicationTests {
 
 	@Test
